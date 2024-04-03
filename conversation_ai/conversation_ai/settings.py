2863017d290e1da718397eed93f4b7902d83e1ed
Gordon Choi <gchoi5738@berkeley.edu>
2024-04-03 12:25:05 -0700

    add csrf trusted origins for app on gcp

--- conversation_ai/conversation_ai/settings.py
+++ conversation_ai/conversation_ai/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://localhost:8080', 'https://conversation-ai-4ijdjf7hua-uc.a.run.app']
